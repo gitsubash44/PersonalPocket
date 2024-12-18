d0b4b386e4315a5758a4d34a8ec4dfdb0ed90de4
gitsubash44 <dhamisubash866@gmail.com>
2024-12-18 19:20:05 +0545

    Add project files.

---
+++ PersonalPocket/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalPocket
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
