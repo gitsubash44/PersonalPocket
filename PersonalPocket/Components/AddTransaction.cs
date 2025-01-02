9a588396578583b4f2662653babc641046b7b28b
gitsubash44 <dhamisubash866@gmail.com>
2025-01-02 16:44:34 +0545

    Adding code for Add tranaction only

---
+++ PersonalPocket/Components/AddTransaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalPocket.Components
+{
+    public class AddTransaction
+    {
+       public string Title { get; set; }
+       public string Type { get; set; }
+       public decimal Amount { get; set; }
+       public DateTime Date { get; set; }
+       public string Tags { get; set; }
+       public string Notes { get; set; }
+        
+    }
+}
