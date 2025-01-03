3eb6983544d325c2d59ad1558a4aaed1b69019fc
gitsubash44 <dhamisubash866@gmail.com>
2025-01-03 17:47:56 +0545

    adding code for Adding transaction

---
+++ PersonalPocket/Components/TransactionModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalPocket.Components
+{
+    public class TransactionModel
+    {
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Type is required.")]
+        public string Type { get; set; }
+
+        [Required(ErrorMessage = "Amount is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than zero.")]
+        public decimal Amount { get; set; }
+
+        [Required(ErrorMessage = "Date is required.")]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Tag is required.")]
+        public string Tag { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
