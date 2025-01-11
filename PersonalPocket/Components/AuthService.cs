f696c76ff880a4227cd22d8b8060a0a2297d817e
gitsubash44 <dhamisubash866@gmail.com>
2025-01-12 00:18:09 +0545

    adding code for Filter, total balance and other balance section

---
+++ PersonalPocket/Components/AuthService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalPocket.Components
+{
+    public class AuthService
+    {
+        public bool IsLoggedIn { get; private set; } = false;
+        public string LoggedInUser { get; private set; } = string.Empty;
+        public string UserCurrency { get; private set; } = string.Empty;
+
+        public Task Login(string username, string currency)
+        {
+            IsLoggedIn = true;
+            LoggedInUser = username;
+            UserCurrency = currency;
+            return Task.CompletedTask;
+        }
+
+        public Task Logout()
+        {
+            IsLoggedIn = false;
+            LoggedInUser = string.Empty;
+            UserCurrency = string.Empty;
+            return Task.CompletedTask;
+        }
+    }
+
+}
---
+++ PersonalPocket/Components/FilteredTransactions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalPocket.Components
+{
+    public class TransactionService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TransactionService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<TransactionModel>> GetTransactionsAsync()
+        {
+            // Assuming you are calling an API endpoint to fetch transactions
+            var response = await _httpClient.GetFromJsonAsync<List<TransactionModel>>("api/transactions");
+
+            // Return the list of transactions
+            return response ?? new List<TransactionModel>();
+        }
+    }
+
+}
